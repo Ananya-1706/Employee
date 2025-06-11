7dfe21775ddf942f7130a853ba18ab6f7e387465
anweshasathua0513 <anweshasathua0513@gmail.com>
2025-06-11 13:10:52 +0530

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,25 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimations(),
+    importProvidersFrom(
+      ReactiveFormsModule,
+      MatButtonModule,
+      MatIconModule,
+      MatTooltipModule
+    )
+  ]
 };
