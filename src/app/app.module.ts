c85b680c9020edba9bc3f0484f5fea2b3b6e79e2
anweshasathua0513 <anweshasathua0513@gmail.com>
2025-06-11 13:11:19 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
 
+// Angular Material Modules
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+
 @NgModule({
   declarations: [
-    AppComponent,
-    EmployeeComponent
+    // AppComponent,
+    // EmployeeComponent
+     
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    AppComponent,
+    EmployeeComponent
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
 export class AppModule { }
