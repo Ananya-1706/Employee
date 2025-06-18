e149ffbc33c21e9083fd7505d9f5210f28363985
anweshasathua0513 <anweshasathua0513@gmail.com>
2025-06-18 18:25:17 +0530

    Update app.component.ts

--- employee-rating-dashboard/src/app/app.component.ts
+++ employee-rating-dashboard/src/app/app.component.ts
@@ -2,40 +2,49 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { HttpClient,HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
    standalone: true,
-   imports: [CommonModule,FormsModule, NgxChartsModule],
+   imports: [CommonModule,FormsModule, NgxChartsModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   employeeName: string = '';
   showChart = false;
+  data: any[] = [];
+
     colorScheme: any = { domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA'] };
   
-    data = [
-    { month: 'January', managerA: 3, managerB: 4 },
-    { month: 'February', managerA: 4, managerB: 5 },
-    { month: 'March', managerA: 5, managerB: 4 }
-  ];
+    
 
   barChartData: any[] = [];
 
-  
+  constructor(private http: HttpClient) {}
 
 
   submitName() {
     if (!this.employeeName.trim()) return;
 
-    this.generateChartData();
-    this.showChart = true;
+    this.http.get<any[]>(`http://localhost:8080/employee-ratings/${this.employeeName}`)
+      .subscribe({
+        next: (response) => {
+          console.log('📥 Data fetched from backend:', response);
+
+          this.data = response;
+          this.generateChartData();
+          this.showChart = true;
+        },
+        error: (err) => {
+          console.error('❌ Failed to fetch data:', err);
+          alert('Failed to fetch employee data. Please check the backend.');
+        }
+      });
   }
 
   generateChartData() {
-    const months = this.data.map(d => d.month);
-
     const managerAData = this.data.map(d => d.managerA);
     const managerBData = this.data.map(d => d.managerB);
 
