b39dc7ac0b7ea26cced20506e498b08f54b9e420
anweshasathua0513 <anweshasathua0513@gmail.com>
2025-06-24 16:27:26 +0530

    Delete src directory

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import { EmployeeComponent } from './app/employee/employee.component';
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
-  bootstrapApplication(EmployeeComponent)
-  .catch(err => console.error(err));
