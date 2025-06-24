17065a7da8cf6be56ae0436f7ddadd088771a4ed
anweshasathua0513 <anweshasathua0513@gmail.com>
2025-06-24 16:41:50 +0530

    Update main.ts

--- Employee-main_any/Employee-main/src/main.ts
+++ Employee-main_any/Employee-main/src/main.ts
@@ -2,8 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { EmployeeComponent } from './app/employee/employee.component';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
-  bootstrapApplication(EmployeeComponent)
+bootstrapApplication(EmployeeComponent)
   .catch(err => console.error(err));
+bootstrapApplication(EmployeeComponent, {
+  providers: [
+    provideHttpClient()
+  ]
+});
